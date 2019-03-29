8af78f90a439cfc469e0c16074d8ef6a94c99400
Andrei Keino <andreikeino@gmail.com>
2019-03-29 16:52:04 +0700

    8

---
+++ Bayesian_Statistics_Techniques_and_Models/week 3/8 ANOVA/quiz_8.R
@@ -0,0 +1,110 @@
+data("PlantGrowth")
+?PlantGrowth
+head(PlantGrowth)
+
+boxplot(weight ~ group, data=PlantGrowth)
+
+library("rjags")
+
+mod_string = " model {
+    for (i in 1:length(y)) {
+y[i] ~ dnorm(mu[grp[i]], prec[grp[i]])
+}
+
+for (j in 1:3) {
+mu[j] ~ dnorm(0.0, 1.0/1.0e6)
+prec[j] ~ dgamma(5/2.0, 5*1.0/2.0)
+sig[j] = sqrt( 1.0 / prec[j] )
+
+}
+
+} "
+
+set.seed(82)
+str(PlantGrowth)
+data_jags = list(y=PlantGrowth$weight, 
+                 grp=as.numeric(PlantGrowth$group))
+
+params = c("mu", "sig")
+
+inits = function() {
+    inits = list("mu"=rnorm(3,0.0,100.0), "prec"=rgamma(3,1.0,1.0))
+}
+
+mod = jags.model(textConnection(mod_string), data=data_jags, inits=inits, n.chains=3)
+update(mod, 1e3)
+
+mod_sim = coda.samples(model=mod,
+                       variable.names=params,
+                       n.iter=5e3)
+mod_csim = as.mcmc(do.call(rbind, mod_sim)) # combined chains
+
+plot(mod_sim)
+
+gelman.diag(mod_sim)
+autocorr.diag(mod_sim)
+effectiveSize(mod_sim)
+
+(pm_params = colMeans(mod_csim))
+
+yhat = pm_params[1:3][data_jags$grp]
+resid = data_jags$y - yhat
+plot(resid)
+
+plot(yhat, resid)
+
+summary(mod_sim)
+
+HPDinterval(mod_csim)
+
+## ***********************************************************************************************
+## old model
+## ***********************************************************************************************
+
+mod_string = " model {
+    for (i in 1:length(y)) {
+y[i] ~ dnorm(mu[grp[i]], prec)
+}
+
+for (j in 1:3) {
+mu[j] ~ dnorm(0.0, 1.0/1.0e6)
+}
+
+prec ~ dgamma(5/2.0, 5*1.0/2.0)
+sig = sqrt( 1.0 / prec )
+} "
+
+set.seed(82)
+str(PlantGrowth)
+data_jags = list(y=PlantGrowth$weight, 
+                 grp=as.numeric(PlantGrowth$group))
+
+params = c("mu", "sig")
+
+inits = function() {
+    inits = list("mu"=rnorm(3,0.0,100.0), "prec"=rgamma(1,1.0,1.0))
+}
+
+mod2 = jags.model(textConnection(mod_string), data=data_jags, inits=inits, n.chains=3)
+update(mod2, 1e3)
+
+mod_old = coda.samples(model=mod2,
+                       variable.names=params,
+                       n.iter=5e3)
+mod_old = as.mcmc(do.call(rbind, mod_sim)) # combined chains
+
+iters = 1e4
+
+dic2 <- dic.samples(mod, n.iter=iters)
+dic1 <- dic.samples(mod2, n.iter=iters)
+ans_4 <- dic1 - dic2
+ans_4
+ans_4 <- diffdic(dic1, dic2)
+ans_4
+
+print("answer 5:")
+HPDinterval(mod_csim)
+
+
+ans_6 <- HPDinterval(mod_csim[, 3] - mod_csim[, 1])
+ans_6
